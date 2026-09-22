d2b8a11df80bf5f97ecba4cb23913c792e16c0f9
Cinta Mutiara <sakianhcinta37@gmail.com>
2026-09-22 12:20:46 +0700

    gogreen new

--- app/Http/Controllers/gogreen.php
+++ app/Http/Controllers/gogreen.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HalamanController extends Controller
+class gogreen extends Controller
 {
     //
     public function gogreen()
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HalamanController;
+use App\Http\Controllers\gogreen;
 use Illuminate\Suport\Facades\Routes;
 
-Route::get('/go-green', [HalamanController::class, 'gogreen']);
+Route::get('/go-green', [gogreen::class, 'gogreen']);
